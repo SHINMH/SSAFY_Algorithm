5fba37fcdd4aaa573345c7164f0ea9fb2c209b99
donggillee-dev <donggillee94@gmail.com>
2021-02-16 23:22:14 +0900

    last update...

--- LinkedList/BOJ_2346/parksr.java
+++ LinkedList/BOJ_2346/parksr.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class ParkSeRyeong {
+public class parksr {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
